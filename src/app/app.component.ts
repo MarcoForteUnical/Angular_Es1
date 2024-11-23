2287d45b74f0fbe6bd7cd3914d40d6f0c13aa21f
Marco Forte <m.forte@evo-bi.com>
2024-11-23 19:49:12 +0100

    esercitazione 1 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TestComponent1Component} from "./test-component-1/test-component-1.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , TestComponent1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/group-1/test-component-2/test-component-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-component-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-component-2.component.html',
+  styleUrl: './test-component-2.component.css'
+})
+export class TestComponent2Component {
+
+}
